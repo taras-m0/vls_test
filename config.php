3647915fee232f06e6072e7840982b415a07edf6
ciscofan <krylov123@gmail.com>
2015-09-29 17:11:37 +0300

    Major: Upload all tasks to github

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+$arrConfig = array(
+    "dbhost" => 'localhost',
+    "dbname" => 'vlstest',
+    "dbuser" => 'username',
+    "dbpass" => 'password',
+    "dbcharset" => 'utf8',
+    "dbcollation" => 'utf8_general_ci');
+
+?>
